62a1f4024a08099051e63d5f503bb6ab87e9b073
taimoor <taimoor.changaiz@nxb.com.pk>
2018-05-22 17:42:31 +0500

    restaurants model test

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,13 +1,18 @@
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-  test 'validate name, address and city' do
-    restaurant = Restaurant.new
+  test 'name cannot be blank' do
+    restaurant = Restaurant.new(address: 'address', city: 'test')
     assert_not restaurant.save
-
-    assert_equal restaurant.errors.count, 3
-    assert_equal restaurant.errors[:name][0], "can't be blank"
-    assert_equal restaurant.errors[:address][0], "can't be blank"
-    assert_equal restaurant.errors[:city][0], "can't be blank"
+    assert_equal restaurant.errors.count, 1
+    assert_equal restaurant.errors[:name][0], "can't be blank"   
   end
+
+  test 'address cannot be blank' do
+    restaurant = Restaurant.new(name: 'name', city: 'test2')
+    assert_not restaurant.save
+    assert_equal restaurant.errors.count, 1
+    assert_equal restaurant.errors[:address][0], "can't be blank"
+  end
+
 end
